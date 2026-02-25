0c37bf18c59b63daedfd59f548726ef46715060f
lerochka <mickeviclera@gmail.com>
2026-02-25 11:41:44 +0700

    Add files via upload

---
+++ lab01/CarsApp w out builder/CarsApp/CarsApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CarsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBoxRacers = new ComboBox();
+            picRacer = new PictureBox();
+            lblStats = new Label();
+            btnBuild = new Button();
+            ((System.ComponentModel.ISupportInitialize)picRacer).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBoxRacers
+            // 
+            comboBoxRacers.FormattingEnabled = true;
+            comboBoxRacers.Items.AddRange(new object[] { "Молния Маккуин", "Круз Рамирес", "Док Хадсон", "Мэтр", "Джексон Шторм", "Франческо Бернулли" });
+            comboBoxRacers.Location = new Point(685, 23);
+            comboBoxRacers.Name = "comboBoxRacers";
+            comboBoxRacers.Size = new Size(151, 28);
+            comboBoxRacers.TabIndex = 0;
+            comboBoxRacers.SelectedIndexChanged += comboBoxRacers_SelectedIndexChanged;
+            // 
+            // picRacer
+            // 
+            picRacer.Location = new Point(12, 11);
+            picRacer.Name = "picRacer";
+            picRacer.Size = new Size(663, 514);
+            picRacer.SizeMode = PictureBoxSizeMode.Zoom;
+            picRacer.TabIndex = 1;
+            picRacer.TabStop = false;
+            // 
+            // lblStats
+            // 
+            lblStats.AutoSize = true;
+            lblStats.Location = new Point(685, 91);
+            lblStats.Name = "lblStats";
+            lblStats.Size = new Size(120, 20);
+            lblStats.TabIndex = 2;
+            lblStats.Text = "Характеристики";
+            // 
+            // btnBuild
+            // 
+            btnBuild.Location = new Point(730, 467);
+            btnBuild.Name = "btnBuild";
+            btnBuild.Size = new Size(94, 29);
+            btnBuild.TabIndex = 3;
+            btnBuild.Text = "Ехала";
+            btnBuild.UseVisualStyleBackColor = true;
+            btnBuild.Click += btnBuild_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(908, 537);
+            Controls.Add(btnBuild);
+            Controls.Add(lblStats);
+            Controls.Add(picRacer);
+            Controls.Add(comboBoxRacers);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)picRacer).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBoxRacers;
+        private PictureBox picRacer;
+        private Label lblStats;
+        private Button btnBuild;
+    }
+}
---
+++ lab01/CarsApp w out builder/CarsApp/CarsApp/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RustezeRacing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            picRacer.SizeMode = PictureBoxSizeMode.Zoom;
+
+            string startPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images/start.png");
+            if (File.Exists(startPath))
+            {
+                using (var stream = new FileStream(startPath, FileMode.Open, FileAccess.Read))
+                {
+                    picRacer.Image = Image.FromStream(stream);
+                }
+            }
+        }
+
+        private RaceParticipant GetSelectedRacer()
+        {
+            if (comboBoxRacers.SelectedItem == null) return null;
+            string selection = comboBoxRacers.SelectedItem.ToString();
+
+            switch (selection)
+            {
+                case "Молния Маккуин": return new McQueen();
+                case "Мэтр": return new Mater();
+                case "Круз Рамирес": return new Cruz();
+                case "Джексон Шторм": return new Storm();
+                case "Док Хадсон": return new Hudson();
+                case "Франческо Бернулли": return new Bernulli();
+                default: return null;
+            }
+        }
+
+        private void UpdateDisplay(RaceParticipant racer)
+        {
+            lblStats.Text = $"Гонщик: {racer.Name}\n" +
+                            $"Опыт: {racer.YearsExp} лет\n" +
+                            $"Мощность: {racer.Power} л.с.\n" +
+                            $"Настройка: {racer.Setting}\n" +
+                            $"Дизайн: {(racer.SponsorDesign ? "С логотипом" : "Базовый")}";
+
+            if (File.Exists(racer.ImagePath))
+            {
+                using (var stream = new FileStream(racer.ImagePath, FileMode.Open, FileAccess.Read))
+                {
+                    picRacer.Image = Image.FromStream(stream);
+                }
+            }
+        }
+
+        private void comboBoxRacers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var racer = GetSelectedRacer();
+            if (racer != null)
+            {
+                racer.Configure();
+                UpdateDisplay(racer);
+            }
+        }
+
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            var racer = GetSelectedRacer();
+            if (racer == null) return;
+
+            racer.Configure();
+            racer.CheckDesign();
+            UpdateDisplay(racer);
+        }
+    }
+
+
+    public abstract class RaceParticipant
+    {
+        public string Name { get; set; }
+        public string Setting { get; set; }
+        public int Power { get; set; }
+        public bool SponsorDesign { get; set; }
+        public int YearsExp { get; set; }
+        public string ImagePath { get; set; }
+
+        public RaceParticipant(string name, int yearsExp)
+        {
+            Name = name;
+            YearsExp = yearsExp;
+        }
+
+        public abstract void Configure();
+
+        protected void SetImagePath(string fileName)
+        {
+            ImagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images", fileName);
+        }
+
+        public void CheckDesign()
+        {
+            if (!SponsorDesign)
+            {
+                SponsorDesign = true;
+                ImagePath = ImagePath.Replace("1.png", "2.png");
+            }
+        }
+    }
+
+    public class McQueen : RaceParticipant
+    {
+        public McQueen() : base("Молния Маккуин", 15) { }
+        public override void Configure()
+        {
+            Power = 750; Setting = "Универсал"; SponsorDesign = true;
+            SetImagePath("mcqueen1.png");
+        }
+    }
+    public class Mater : RaceParticipant
+    {
+        public Mater() : base("Мэтр", 10) { }
+        public override void Configure()
+        {
+            Power = 300; Setting = "Грунтовая дорога"; SponsorDesign = false;
+            SetImagePath("mater1.png");
+        }
+    }
+
+    public class Cruz : RaceParticipant
+    {
+        public Cruz() : base("Круз Рамирес", 3) { }
+        public override void Configure()
+        {
+            Power = 900; Setting = "Гоночный трек"; SponsorDesign = false;
+            SetImagePath("cruz1.png");
+        }
+    }
+
+    public class Hudson : RaceParticipant
+    {
+        public Hudson() : base("Док Хадсон", 40) { }
+        public override void Configure()
+        {
+            Power = 450; Setting = "Универсал"; SponsorDesign = false;
+            SetImagePath("doc1.png");
+        }
+    }
+
+    public class Storm : RaceParticipant
+    {
+        public Storm() : base("Джексон Шторм", 3) { }
+        public override void Configure()
+        {
+            Power = 900; Setting = "Гоночный трек"; SponsorDesign = false;
+            SetImagePath("storm1.png");
+        }
+    }
+
+    public class Bernulli : RaceParticipant
+    {
+        public Bernulli() : base("Франческо Бернулли", 15) { }
+        public override void Configure()
+        {
+            Power = 750; Setting = "Гоночный трек"; SponsorDesign = false;
+            SetImagePath("bernulli1.png");
+        }
+    }
+}
